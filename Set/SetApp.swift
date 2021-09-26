b36e774791bbe6d8b89798a5f517153bad500491
Kris Laratta <krislaratta@gmail.com>
2021-09-25 22:31:26 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Kris Laratta on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
